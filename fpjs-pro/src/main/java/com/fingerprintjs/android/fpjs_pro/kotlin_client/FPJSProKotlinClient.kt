b808bfe27000b557c9646415cd97016aae2d202f
Alexey-Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2021-11-29 13:00:46 +0300

    Add background execution

--- fpjs-pro/src/main/java/com/fingerprintjs/android/fpjs_pro/kotlin_client/FPJSProKotlinClient.kt
+++ fpjs-pro/src/main/java/com/fingerprintjs/android/fpjs_pro/kotlin_client/FPJSProKotlinClient.kt
@@ -2,11 +2,14 @@ package com.fingerprintjs.android.fpjs_pro.kotlin_client
 
 
 import com.fingerprintjs.android.fpjs_pro.FPJSProClient
+import java.util.concurrent.Executors
 
 
 class FPJSProKotlinClient(
     private val interactor: FetchVisitorIdInteractor
 ) : FPJSProClient {
+
+    private val executor = Executors.newSingleThreadExecutor()
 
     override fun getVisitorId(listener: (String) -> (Unit)) {
         getVisitorId(emptyMap(), listener, {})
@@ -21,12 +24,14 @@ class FPJSProKotlinClient(
         listener: (String) -> Unit,
         errorListener: (String) -> Unit
     ) {
-        interactor.getVisitorId(tags,
-            {
-                listener.invoke(it)
-            },
-            {
-                errorListener.invoke(it)
-            })
+        executor.execute {
+            interactor.getVisitorId(tags,
+                {
+                    listener.invoke(it)
+                },
+                {
+                    errorListener.invoke(it)
+                })
+        }
     }
 }
