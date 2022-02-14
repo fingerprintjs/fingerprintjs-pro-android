2edb748adf1d09e1c17247e2636f50b6d187994c
Alexey Verkhovsky <alexey.verkhovsky@fingerprintjs.com>
2022-02-14 15:06:32 +0300

    Make all JSON parameters optional

--- fpjs-pro/src/main/java/com/fingerprintjs/android/fpjs_pro/kotlin_client/FetchVisitorIdRequest.kt
+++ fpjs-pro/src/main/java/com/fingerprintjs/android/fpjs_pro/kotlin_client/FetchVisitorIdRequest.kt
@@ -1,198 +1,17 @@
 package com.fingerprintjs.android.fpjs_pro.kotlin_client
 
 
+import android.os.Build
+import android.util.Log
 import com.fingerprintjs.android.fpjs_pro.kotlin_client.http_client.Request
 import com.fingerprintjs.android.fpjs_pro.kotlin_client.http_client.RequestResultType
 import com.fingerprintjs.android.fpjs_pro.kotlin_client.http_client.RequestType
 import com.fingerprintjs.android.fpjs_pro.kotlin_client.http_client.TypedRequestResult
+import org.json.JSONArray
 import org.json.JSONObject
 import java.util.*
 import kotlin.collections.HashMap
 
-
-//region: Response Types
-
-data class IpLocation(
-    val accuracyRadius: Int,
-    val latitude: Double,
-    val longitude: Double,
-    val postalCode: String,
-    val timezone: String,
-    val city: City,
-    val country: Country,
-    val subdivisions: List<Subdivisions>
-
-) {
-
-    data class City(
-        val name: String
-    )
-
-    data class Country(
-        val code: String,
-        val name: String
-    )
-
-    data class Continent(
-        val code: String,
-        val name: String
-    )
-
-    data class Subdivisions(
-        val isoCode: String,
-        val name: String
-    )
-}
-
-data class ConfidenceScore(
-    val score: Double
-)
-
-data class FetchVisitorIdResponse(
-    val requestId: String,
-    val visitorId: String,
-    val visitorFound: Boolean,
-    val ipAddress: String,
-    val ipLocation: IpLocation,
-    val osName: String,
-    val osVersion: String,
-    val errorMessage: String? = ""
-)
-
-//endregion
-
-//region: Parse response
-
-class FetchVisitorIdResult(
-    type: RequestResultType,
-    rawResponse: ByteArray?,
-    private val extendedFormat: Boolean
-) : TypedRequestResult<FetchVisitorIdResponse>(type, rawResponse) {
-    override fun typedResult(): FetchVisitorIdResponse {
-        val body = rawResponse?.toString(Charsets.UTF_8) ?: return emptyResponse(
-            UNKNOWN_KEY,
-            rawResponse?.toString(Charsets.UTF_8) ?: UNKNOWN_KEY
-        )
-
-        var requestId = ""
-        return try {
-            val jsonBody = JSONObject(body)
-
-            requestId = jsonBody.getString(REQUEST_ID_KEY)
-
-            val result = jsonBody
-                .getJSONObject(PRODUCTS_KEY)
-                .getJSONObject(IDENTIFICATION_KEY)
-                .getJSONObject(DATA_KEY)
-                .getJSONObject(RESULT_KEY)
-
-            val deviceId =
-                result.getString(VISITOR_ID_KEY)
-
-            val visitorFound = result.getBoolean(VISITOR_FOUND_KEY)
-            val ipAddress = result.getString(IP_KEY)
-            val ipLocation = parseIpLocation(result)
-            val osName = result.getString(OS_KEY)
-            val osVersion = result.getString(OS_VERSION_KEY)
-            FetchVisitorIdResponse(
-                requestId,
-                deviceId,
-                visitorFound,
-                ipAddress,
-                ipLocation,
-                osName,
-                osVersion
-            )
-        } catch (exception: Exception) {
-            emptyResponse(
-                requestId,
-                "RequestID: $requestId " + rawResponse.toString(Charsets.UTF_8)
-            )
-        }
-    }
-
-    private fun parseConfidenceScore(resultJson: JSONObject): ConfidenceScore {
-        return ConfidenceScore(
-            resultJson.getJSONObject(CONFIDENCE_KEY).getDouble(CONFIDENCE_SCORE_KEY)
-        )
-    }
-
-    private fun parseIpLocation(resultJson: JSONObject): IpLocation {
-        val ipLocationJson = resultJson.getJSONObject(IP_LOCATION_KEY)
-        val accuracyRadius = resultJson.getInt(ACCURACY_RADIUS_KEY)
-        val latitude = resultJson.getDouble(LATITUDE_KEY)
-        val longitude = resultJson.getDouble(LONGITUDE_KEY)
-        val postalCode = resultJson.getString(POSTAL_CODE_KEY)
-        val timezone = resultJson.getString(TIMEZONE_KEY)
-
-        val cityJson = ipLocationJson.getJSONObject(CITY_KEY)
-        val city = IpLocation.City(cityJson.getString(CITY_NAME_KEY))
-
-        val countryJson = ipLocationJson.getJSONObject(COUNTRY_KEY)
-        val country = IpLocation.Country(
-            countryJson.getString(COUNTRY_CODE_KEY),
-            countryJson.getString(COUNTRY_NAME_KEY)
-        )
-
-        val continentJson = ipLocationJson.getJSONObject(CONTINENT_KEY)
-        val continent = IpLocation.Continent(
-            continentJson.getString(CONTINENT_CODE_KEY),
-            continentJson.getString(CONTINENT_NAME_KEY)
-        )
-
-        val subdivisionsJson = ipLocationJson.getJSONArray(SUBDIVISIONS_KEY)
-        val subdivisions = LinkedList<IpLocation.Subdivisions>()
-
-        for (i in 0..subdivisionsJson.length()) {
-            subdivisions.add(
-                IpLocation.Subdivisions(
-                    subdivisionsJson.getJSONObject(i).getString(ISO_CODE_KEY),
-                    subdivisionsJson.getJSONObject(i).getString(SUBDIVISION_NAME_KEY)
-                )
-            )
-        }
-
-        return IpLocation(
-            accuracyRadius,
-            latitude,
-            longitude,
-            postalCode,
-            timezone,
-            city,
-            country,
-            subdivisions
-        )
-    }
-
-    private fun emptyResponse(requestId: String, errorMessage: String): FetchVisitorIdResponse {
-        return FetchVisitorIdResponse(
-            requestId,
-            UNKNOWN_KEY,
-            visitorFound = false,
-            ipAddress = UNKNOWN_KEY,
-            ipLocation = IpLocation(
-                0,
-                0.0,
-                0.0,
-                UNKNOWN_KEY,
-                UNKNOWN_KEY,
-                IpLocation.City(
-                    UNKNOWN_KEY
-                ),
-                IpLocation.Country(
-                    UNKNOWN_KEY,
-                    UNKNOWN_KEY
-                ),
-                emptyList()
-            ),
-            UNKNOWN_KEY,
-            UNKNOWN_KEY,
-            errorMessage = errorMessage
-        )
-    }
-}
-
-//endregion
 
 //region: Request
 class FetchVisitorIdRequest(
@@ -271,6 +90,162 @@ class FetchVisitorIdRequest(
 
 //endregion
 
+
+//region: Parse response
+
+class FetchVisitorIdResult(
+    type: RequestResultType,
+    rawResponse: ByteArray?,
+    private val extendedFormat: Boolean
+) : TypedRequestResult<FetchVisitorIdResponse>(type, rawResponse) {
+    override fun typedResult(): FetchVisitorIdResponse {
+        val body = rawResponse?.toString(Charsets.UTF_8) ?: return emptyResponse(
+            UNKNOWN_KEY,
+            rawResponse?.toString(Charsets.UTF_8) ?: UNKNOWN_KEY
+        )
+
+        var requestId = ""
+        return try {
+            val jsonBody = JSONObject(body)
+
+            requestId = jsonBody.getString(REQUEST_ID_KEY)
+
+            val result = jsonBody
+                .getJSONObject(PRODUCTS_KEY)
+                .getJSONObject(IDENTIFICATION_KEY)
+                .getJSONObject(DATA_KEY)
+                .getJSONObject(RESULT_KEY)
+
+            val deviceId =
+                result.getString(VISITOR_ID_KEY)
+
+            val confidenceScore = parseConfidenceScore(result)
+
+            var visitorFound = false
+            var ipAddress = UNKNOWN_KEY
+            var ipLocation: IpLocation? = null
+            var osName = UNKNOWN_KEY
+            var osVersion = UNKNOWN_KEY
+
+            if (extendedFormat) {
+                visitorFound = result.optBoolean(VISITOR_FOUND_KEY, false)
+                ipAddress = result.optString(IP_KEY, UNKNOWN_KEY)
+                ipLocation = parseIpLocation(result)
+                osName = result.optString(OS_KEY, "Android")
+                osVersion = result.optString(OS_VERSION_KEY, Build.VERSION.CODENAME)
+            }
+            FetchVisitorIdResponse(
+                requestId,
+                deviceId,
+                confidenceScore,
+                visitorFound,
+                ipAddress,
+                ipLocation,
+                osName,
+                osVersion
+            )
+        } catch (exception: Exception) {
+            exception.printStackTrace()
+            Log.e(this.javaClass.canonicalName, exception.toString())
+            emptyResponse(
+                requestId,
+                "RequestID: $requestId " + rawResponse.toString(Charsets.UTF_8)
+            )
+        }
+    }
+
+    private fun parseConfidenceScore(resultJson: JSONObject): ConfidenceScore {
+        return ConfidenceScore(
+            resultJson.optJSONObject(CONFIDENCE_KEY)?.optDouble(CONFIDENCE_SCORE_KEY, 0.0) ?: 0.0
+        )
+    }
+
+    private fun parseIpLocation(resultJson: JSONObject): IpLocation {
+        val ipLocationJson = resultJson.optJSONObject(IP_LOCATION_KEY)
+        val accuracyRadius = ipLocationJson?.optInt(ACCURACY_RADIUS_KEY, 0) ?: 0
+        val latitude = ipLocationJson?.optDouble(LATITUDE_KEY, 0.0) ?: 0.0
+        val longitude = ipLocationJson?.optDouble(LONGITUDE_KEY, 0.0) ?: 0.0
+        val postalCode = ipLocationJson?.optString(POSTAL_CODE_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY
+        val timezone = ipLocationJson?.optString(TIMEZONE_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY
+
+        val cityJson = ipLocationJson.optJSONObject(CITY_KEY)
+        val city = IpLocation.City(cityJson?.optString(CITY_NAME_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY)
+
+        val countryJson = ipLocationJson?.getJSONObject(COUNTRY_KEY)
+        val country = IpLocation.Country(
+            countryJson?.optString(COUNTRY_CODE_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY,
+            countryJson?.optString(COUNTRY_NAME_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY
+        )
+
+        val continentJson = ipLocationJson.optJSONObject(CONTINENT_KEY)
+        val continent = IpLocation.Continent(
+            continentJson?.optString(CONTINENT_CODE_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY,
+            continentJson?.optString(CONTINENT_NAME_KEY, UNKNOWN_KEY) ?: UNKNOWN_KEY
+        )
+
+        val subdivisionsJson = ipLocationJson.optJSONArray(SUBDIVISIONS_KEY) ?: JSONArray(emptyArray<JSONObject>())
+        val subdivisions = LinkedList<IpLocation.Subdivisions>()
+
+        for (i in 0 until subdivisionsJson.length()) {
+            subdivisions.add(
+                IpLocation.Subdivisions(
+                    subdivisionsJson.optJSONObject(i).optString(ISO_CODE_KEY, UNKNOWN_KEY),
+                    subdivisionsJson.optJSONObject(i).optString(SUBDIVISION_NAME_KEY, UNKNOWN_KEY)
+                )
+            )
+        }
+
+        return IpLocation(
+            accuracyRadius,
+            latitude,
+            longitude,
+            postalCode,
+            timezone,
+            city,
+            country,
+            continent,
+            subdivisions
+        )
+    }
+
+    private fun emptyResponse(requestId: String, errorMessage: String): FetchVisitorIdResponse {
+        return FetchVisitorIdResponse(
+            requestId,
+            UNKNOWN_KEY,
+            confidenceScore = ConfidenceScore(0.0),
+            visitorFound = false,
+            ipAddress = UNKNOWN_KEY,
+            ipLocation = IpLocation(
+                0,
+                0.0,
+                0.0,
+                UNKNOWN_KEY,
+                UNKNOWN_KEY,
+                IpLocation.City(
+                    UNKNOWN_KEY
+                ),
+                IpLocation.Country(
+                    UNKNOWN_KEY,
+                    UNKNOWN_KEY
+                ),
+                IpLocation.Continent(
+                    UNKNOWN_KEY, UNKNOWN_KEY
+                ),
+                emptyList()
+
+            ),
+            UNKNOWN_KEY,
+            UNKNOWN_KEY,
+            errorMessage = errorMessage
+        )
+    }
+
+    private fun emptyJson() = JSONObject(emptyMap<String, String>())
+}
+
+//endregion
+
+
 //region: Const
 
 private const val PRODUCTS_KEY = "products"
@@ -292,7 +267,7 @@ private const val POSTAL_CODE_KEY = "postalCode"
 private const val TIMEZONE_KEY = "timezone"
 
 private const val CITY_KEY = "city"
-private const val CITY_NAME_KEY = "city"
+private const val CITY_NAME_KEY = "name"
 
 private const val COUNTRY_KEY = "country"
 private const val COUNTRY_CODE_KEY = "code"
